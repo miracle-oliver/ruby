053f471e461e514acfe4f5eecc9ad7c55688b8f3
Alozie Miracle Patrick <nacotech09@gmail.com>
2025-02-04 23:41:13 +0100

    documenting task and implentation strategy

---
+++ index.rb
@@ -0,0 +1,6 @@
+# Task 
+# implement a Tax Calculator in Ruby that determines the correct tax rate for different transactions based on the buyer's location and product/service type.
+
+# Plan for Implementation
+# Define a TaxCalculator class - will take the buyer location, product/service type, and buyer type (individual/company) as inputs
+# methods - Goods (good), Digital Services (service, digital), Onsite Services (service, onsite)
