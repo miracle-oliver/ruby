b1abea0fedc3bf5394188495833253d89a49a691
Alozie Miracle Patrick <nacotech09@gmail.com>
2025-02-04 23:58:06 +0100

    created and initialized the taxcalcultor class

--- index.rb
+++ index.rb
@@ -4,3 +4,29 @@
 # Plan for Implementation
 # Define a TaxCalculator class - will take the buyer location, product/service type, and buyer type (individual/company) as inputs
 # methods - Goods (good), Digital Services (service, digital), Onsite Services (service, onsite)
+
+
+# Standard spanish VAT - declining
+SPANISH_VAT = 21
+
+# List of all EU countries
+EU_COUNTRIES = [
+  "Austria", "Belgium", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark",
+  "Estonia", "Finland", "France", "Germany", "Greece", "Hungary", "Ireland", "Italy",
+  "Latvia", "Lithuania", "Luxembourg", "Malta", "Netherlands", "Poland", "Portugal",
+  "Romania", "Slovakia", "Slovenia", "Spain", "Sweden"
+]
+
+# creating a tasx calculator class
+class TaxCalculator
+  # defining the attributes the class object should accpet as inputs
+  attr_accessor :buyer_location, :product_type, :buyer_type, :service_location
+
+  # initialize the taxcalculator class
+  def initialize(buyer_location, product_type, buyer_type, service_location = nil)
+    @buyer_location = buyer_location
+    @product_type = product_type
+    @buyer_type = buyer_type
+    @service_location = service_location
+  end
+end
